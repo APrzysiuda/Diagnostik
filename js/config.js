84dcdbeb0a627fc304be00ee1209f05d5f36333c
APrzysiuda <56821340+APrzysiuda@users.noreply.github.com>
2021-05-04 10:55:56 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_3dou5LmNH',
+        userPoolClientId: '1o3lnhbb945abbc4a3t1i82jn',
+        region: 'eu-central-1'
+    },
+    api: {
+        invokeUrl: 'https://k2cdp5ols3.execute-api.eu-west-1.amazonaws.com/prod'
+    }
+};
