80136e736ab45087296d132149c58a454826e59a
JeanMax <181727@student.umons.ac.be>
2021-05-06 01:56:58 +0200

    Commit

--- js/s3Script.js
+++ js/s3Script.js
@@ -7,3 +7,6 @@ function rap(link) {
     rapport = document.getElementById('rapport');
     rapport.innerHTML = '<iframe src="rapport.pdf" class ="box_shadow_without_radius" width="80%" height=800  align="middle" frameborder="0" allowfullscreen="True" style="border:0;"></iframe>';
 }
+document.getElementById('rapport').onclick = function() {
+    this.style.display = 'none';
+};
