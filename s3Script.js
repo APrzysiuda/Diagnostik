2d4c99dab2c2979f4f1ce3404b360e8d9719fad8
APrzysiuda <56821340+APrzysiuda@users.noreply.github.com>
2021-05-05 16:19:41 +0200

    Update s3Script.js

--- s3Script.js
+++ s3Script.js
@@ -35,7 +35,17 @@ function s3upload() {
                 Body: file
             }
         });
-        return alert("Promise avt.");
+        s3.putObject(params, function(err, data) {
+
+         if (err) {
+
+             console.log(err)
+
+         } else {
+
+             console.log("Successfully uploaded data to myBucket/myKey");
+
+         }
 
         var promise = upload.promise();
 
