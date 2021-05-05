62c32e3f6146085e987ee47867b65ab73a5c6d1f
JeanMax <181727@student.umons.ac.be>
2021-05-06 01:46:22 +0200

    Commit

--- js/s3Script.js
+++ js/s3Script.js
@@ -4,11 +4,11 @@ function Bidule() {
 };
 
 function displayImg(link) {
-    overlay = document.getElementById('overlay');
+    overlay = document.getElementById('rapport');
     overlay.innerHTML = '<iframe src="rapport.pdf" class ="box_shadow_without_radius" width="80%" height=800  align="middle" frameborder="0" allowfullscreen="True" style="border:0;"></iframe>';
     overlay.style.display = 'block';
 }
-document.getElementById('overlay').onclick = function() {
+document.getElementById('rapport').onclick = function() {
     this.style.display = 'none';
 };
 
