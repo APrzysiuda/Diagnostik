56025ba2b938b030cbd8cc0faa7102e79897588a
JeanMax <181727@student.umons.ac.be>
2021-05-06 01:51:56 +0200

    Commit

--- js/s3Script.js
+++ js/s3Script.js
@@ -1,9 +1,9 @@
 function Bidule() {
-    displayImg(this);
+    rap(this);
     return false;
 };
 
-function displayImg(link) {
+function rap(link) {
     rapport = document.getElementById('rapport');
     rapport.innerHTML = '<iframe src="rapport.pdf" class ="box_shadow_without_radius" width="80%" height=800  align="middle" frameborder="0" allowfullscreen="True" style="border:0;"></iframe>';
 }
