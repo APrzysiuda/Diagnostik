50d70715dea0b39dd5ca850e28b8bb0ce911c817
JeanMax <181727@student.umons.ac.be>
2021-05-06 02:17:00 +0200

    Commit

--- js/s3Script.js
+++ js/s3Script.js
@@ -4,9 +4,17 @@ function Bidule() {
 };
 
 function rap(link) {
-    rapport = document.getElementById('rapport');
-    rapport.innerHTML = '<iframe src="rapport.pdf" class ="box_shadow_without_radius" width="80%" height=800  align="middle" frameborder="0" allowfullscreen="True" style="border:0;"></iframe>';
+    load = document.getElementById('fileUpload');
+    if (load.value.split('.').pop() == 'edf'){
+        rapport = document.getElementById('rapport');
+        rapport.innerHTML = '<iframe src="rapport.pdf" class ="box_shadow_without_radius" width="80%" height=800  align="middle" frameborder="0" allowfullscreen="True" style="border:0;"></iframe>';
+    }
+    else {
+        alert('You have to enter an edf file !')
+    }
+
 }
 document.getElementById('rapport').onclick = function() {
     this.style.display = 'none';
 };
+
